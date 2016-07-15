12cb5784e23ec8c46e717f4600d5a389b0865720
Sergeij Belevskij <bbelevskij@gmail.com>
2016-07-16 01:53:00 +0500

    rubocop & travis is still failed

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -2,8 +2,8 @@ include BrandsHelper
 include ModelsHelper
 # Models controller
 class ModelsController < ApplicationController
-  before_action :set_model_list, only: [:index]
-  before_action :set_model_selected, only: [:show]
+  before_action :load_model_list, only: [:index]
+  before_action :load_model_selected, only: [:show]
   def index
   end
 
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -7,7 +7,7 @@ module SearchHelper
     model_list = []
     parser_settings[:parser_settings].each do |site_settings|
       page = page_load(url: "#{site_settings[:host]}#{site_settings[:search_path].gsub(/<text>/, text)}")
-      models = scan_page_for_model_links(page, model_list, {name: 'all brands'}, site_settings)
+      model_list << scan_page_for_model_links(page, model_list, { name: 'all brands' }, site_settings)
     end
     @model_list = model_list
   end
